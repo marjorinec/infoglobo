5d533987381593739094d2c3d327af59e66af76d
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-11 11:28:22 -0300

    chore: add h1 and its css

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import './header.css'
 
 const Header = () => (
   <header>
     <Row>
-      <Col>
-        Infoglobo
+      <Col className="header" xs={12} md={12} lg={12}>
+        <h1 className="text-uppercase">Infoglobo</h1>
       </Col>
     </Row>
   </header>    
