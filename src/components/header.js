df1cf76c153d591bbb23599669d28c8666be71a4
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-11 11:31:05 -0300

    chore: add header style

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => (
   <header>
     <Row>
       <Col className="header" xs={12} md={12} lg={12}>
-        <h1 className="text-uppercase">Infoglobo</h1>
+        <h1 className="text-uppercase" id="header-title">Infoglobo</h1>
       </Col>
     </Row>
   </header>    
