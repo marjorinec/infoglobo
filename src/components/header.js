eb7561106c8acaac22df38f7deb2328e1f2bde6a
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-13 17:32:10 -0300

    chore: add icon and logo and their style

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,10 @@ const Header = () => (
   <header>
     <Row>
       <Col className="header" xs={12} md={12} lg={12}>
-        <h1 className="text-uppercase" id="header-title">Infoglobo</h1>
+        <img className="burger-button" src="./burger-icon.png"/>
+        <h1 id="header-title">
+          <img id="header-img" src="./infoglobo-logo.png"/>
+        </h1>
       </Col>
     </Row>
   </header>    
