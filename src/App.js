37aff3760a1348cb90d26d172c2caa8a7fa3aa82
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-13 21:22:22 -0300

    chore: refactor app.js and bignews to get data via props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,39 @@ import { Container, Row, Col } from 'react-bootstrap'
 // import './App.css'
 
 function App() {
+  const newsOne = {
+    category: "Brasil",
+    title: "Nullam arcu lacus, tincidunt vel efficitur",
+    description: "Vivamus vitae mauris eget enim gravida eleifend. Nunc et diam ddfsfsd sfdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd",
+    img: "./news-imgs/464x261-1.jpeg"
+  }
+
+  const newsTwo = {
+    category: "Moda",
+    title: "Aenean quis risus vel nulla maximus",
+    description: "Quisque aliquet euismod justo, sit amet sodales eros sagittis u ddfsfsd sfddsdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd",
+    img: "./news-imgs/464x261-2.jpeg"
+  }
+
   return (
     <section className="App">
       <Header />
       <article>
         <Container>
-          <BigNews/>
+          <Row>
+            <BigNews
+              category={newsOne.category}
+              title={newsOne.title}
+              description={newsOne.description}
+              img={newsOne.img}
+            />
+            <BigNews
+              category={newsTwo.category}
+              title={newsTwo.title}
+              description={newsTwo.description}
+              img={newsTwo.img}
+            />
+          </Row>
         </Container>
       </article>
     </section>
--- src/components/BigNews.js
+++ src/components/BigNews.js
@@ -9,42 +9,22 @@ class BigNews extends React.Component {
 
   render() {
     return (
-      <section>
-        <Row>
-          <Col className="first-news" lg={6} md={6}>
-            <div className="news-img-overlay"></div>
-            <img alt="news" className="news-img" src="./news-imgs/464x261-1.jpeg" />
-            <img alt="share" className="share-icon" src="./icons/share.png" />
-            <div className="news-info">
-              <div className="news-category">
-                BRASIL
-              </div>
-              <h5 className="news-title">
-                Nullam arcu lacus, tincidunt vel efficitur
-              </h5>
-              <div className="news-description">
-                Vivamus vitae mauris eget enim gravida eleifend. Nunc et diam ddfsfsd sfdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd
-              </div>
-            </div>
-          </Col>
-          <Col className="first-news" lg={6} md={6}>
-            <div className="news-img-overlay"></div>
-            <img alt="news" className="news-img" src="./news-imgs/464x261-2.jpeg" />
-            <img alt="share" className="share-icon" src="./icons/share.png" />
-            <div className="news-info">
-              <div className="news-category">
-                MODA
-              </div>
-              <h5 className="news-title">
-                Aenean quis risus vel nulla maximus
-              </h5>
-              <div className="news-description">
-              Quisque aliquet euismod justo, sit amet sodales eros sagittis u ddfsfsd sfddsdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </section>
+      <Col className="first-news" lg={6} md={6}>
+        <div className="news-img-overlay"></div>
+        <img alt="news" className="news-img" src={this.props.img} />
+        <img alt="share" className="share-icon" src="./icons/share.png" />
+        <div className="news-info">
+          <div className="news-category text-uppercase">
+            {this.props.category}
+          </div>
+          <h5 className="news-title">
+            {this.props.title}
+          </h5>
+          <div className="news-description">
+            {this.props.description}
+          </div>
+        </div>
+      </Col>
     )
   }
 }
