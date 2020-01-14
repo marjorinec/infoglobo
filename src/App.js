0aca382f97dc42996b5c429ca8ec6d5d5f000bcb
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-13 21:10:28 -0300

    chore: move big news code and its css to a separate component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from './components/header'
+import BigNews from './components/BigNews'
 import { Container, Row, Col } from 'react-bootstrap'
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
@@ -9,42 +10,7 @@ function App() {
       <Header />
       <article>
         <Container>
-            <section>
-              <Row>
-                <Col className="first-news" lg={6} md={6}>
-                  <div className="news-img-overlay"></div>
-                  <img alt="news" className="news-img" src="./news-imgs/464x261-1.jpeg" />
-                  <img alt="share" className="share-icon" src="./icons/share.png" />
-                  <div className="news-info">
-                    <div className="news-category">
-                      BRASIL
-                    </div>
-                    <h5 className="news-title">
-                      Nullam arcu lacus, tincidunt vel efficitur
-                    </h5>
-                    <div className="news-description">
-                      Vivamus vitae mauris eget enim gravida eleifend. Nunc et diam ddfsfsd sfdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd
-                    </div>
-                  </div>
-                </Col>
-                <Col className="first-news" lg={6} md={6}>
-                  <div className="news-img-overlay"></div>
-                  <img alt="news" className="news-img" src="./news-imgs/464x261-2.jpeg" />
-                  <img alt="share" className="share-icon" src="./icons/share.png" />
-                  <div className="news-info">
-                    <div className="news-category">
-                      MODA
-                    </div>
-                    <h5 className="news-title">
-                      Aenean quis risus vel nulla maximus
-                    </h5>
-                    <div className="news-description">
-                    Quisque aliquet euismod justo, sit amet sodales eros sagittis u ddfsfsd sfddsdf sdfsdf sdfsdfsd sdfsdfsd fsdfsdfd fdfdsf fdf dfd
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-            </section>
+          <BigNews/>
         </Container>
       </article>
     </section>
