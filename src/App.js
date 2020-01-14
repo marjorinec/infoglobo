8b9aab7324fa1c34a73f51d2eb1a6de4675d28be
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-14 14:59:11 -0300

    feat: add brazil and world sections

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import BigNews from './components/BigNews'
 import MidNews from './components/MidNews'
 import SmallNews from './components/SmallNews'
 import Propaganda from './components/Propaganda'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import axios from 'axios'
 
 class App extends React.Component {
@@ -20,10 +20,10 @@ class App extends React.Component {
     const infogloboData = await axios.get('/data.json')
 
     this.setState({
-      mainData: infogloboData.data.section[0].data,
-      brazilData: infogloboData.data.section[1].data,
-      worldData: infogloboData.data.section[2].data
-    })
+      mainData: infogloboData.data.section[0],
+      brazilData: infogloboData.data.section[1],
+      worldData: infogloboData.data.section[2]
+    }, () => console.log(this.state.brazilData))
   }
 
   componentDidMount() {
@@ -41,37 +41,80 @@ class App extends React.Component {
           <Container>
             <Row>
               <BigNews
-                newsInfo={this.state.mainData[0]}
+                newsInfo={this.state.mainData.data[0]}
               />
               <BigNews
-                newsInfo={this.state.mainData[1]}
+                newsInfo={this.state.mainData.data[1]}
               />
 
               <MidNews
-                newsInfo={this.state.mainData[2]}
+                newsInfo={this.state.mainData.data[2]}
               />
               <MidNews
-                newsInfo={this.state.mainData[3]}
+                newsInfo={this.state.mainData.data[3]}
               />
             </Row>
 
             <Row>
               <SmallNews
-                newsInfo={this.state.mainData[4]}
+                newsInfo={this.state.mainData.data[4]}
               />
               <SmallNews
-                newsInfo={this.state.mainData[5]}
+                newsInfo={this.state.mainData.data[5]}
               />
               <SmallNews
-                newsInfo={this.state.mainData[6]}
+                newsInfo={this.state.mainData.data[6]}
               />
               <SmallNews
-                newsInfo={this.state.mainData[7]}
+                newsInfo={this.state.mainData.data[7]}
               />
             </Row>
           </Container>
 
           <Propaganda/>
+
+          <Container>
+            <Row className="news">
+              <Col className="section-title text-uppercase">
+                {this.state.brazilData.name}
+              </Col>
+            </Row>
+            <Row className="section-news">
+              <SmallNews
+                newsInfo={this.state.brazilData.data[0]}
+              />
+              <SmallNews
+                newsInfo={this.state.brazilData.data[1]}
+              />
+              <SmallNews
+                newsInfo={this.state.brazilData.data[2]}
+              />
+              <SmallNews
+                newsInfo={this.state.brazilData.data[3]}
+              />
+            </Row>
+
+            <Row className="news">
+              <Col className="section-title text-uppercase">
+                {this.state.worldData.name}
+              </Col>
+            </Row>
+            <Row className="section-news">
+              <SmallNews
+                newsInfo={this.state.worldData.data[0]}
+              />
+              <SmallNews
+                newsInfo={this.state.worldData.data[1]}
+              />
+              <SmallNews
+                newsInfo={this.state.worldData.data[2]}
+              />
+              <SmallNews
+                newsInfo={this.state.worldData.data[3]}
+              />
+            </Row>
+          </Container>
+
         </article>
       </section>
       )
