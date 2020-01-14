9a8f2b30065eeae9f83dbf29e460884d0a0a84dc
Alline Marjorie <allinemarjorie@gmail.com>
2020-01-14 12:40:21 -0300

    chore: add loading conditional to bignews

--- src/components/BigNews.js
+++ src/components/BigNews.js
@@ -19,25 +19,28 @@ class BigNews extends React.Component {
   }
 
   render() {
-
-    return (
-      <Col className="news" lg={6} md={6}>
-        <div className="news-img-overlay"></div>
-        <img alt="news" className="news-img" src={this.state.image} />
-        <img alt="share" className="share-icon" src="./icons/share.png" />
-        <div className="news-info">
-          <div className="news-category text-uppercase">
-            {this.props.newsInfo.label}
+    if (this.state.image === null) {
+      return (<div>Carregando todo</div>)
+    } else {
+      return (
+        <Col className="news" lg={6} md={6}>
+          <div className="news-img-overlay"></div>
+          <img alt="news" className="news-img" src={this.state.image} />
+          <img alt="share" className="share-icon" src="./icons/share.png" />
+          <div className="news-info">
+            <div className="news-category text-uppercase">
+              {this.props.newsInfo.label}
+            </div>
+            <h5 className="news-title">
+              {this.props.newsInfo.title}
+            </h5>
+            <div className="news-description">
+              {this.props.newsInfo.description}
+            </div>
           </div>
-          <h5 className="news-title">
-            {this.props.newsInfo.title}
-          </h5>
-          <div className="news-description">
-            {this.props.newsInfo.description}
-          </div>
-        </div>
-      </Col>
-    )
+        </Col>
+      )
+    }
   }
 }
 
